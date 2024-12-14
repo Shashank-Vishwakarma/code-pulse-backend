930aca6a4874e79d4348f1bd920915bf8d34d3f8
Shashank <shashank78680@gmail.com>
2024-12-14 17:46:33 +0530

    env config setup, db connection

--- config/config.go
+++ config/config.go
@@ -1,1 +1,53 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+var Config *Env
+
+type Env struct {
+	PORT           string `mapstructure:"PORT"`
+	DATABASE_URL   string `mapstructure:"DATABASE_URL"`
+	JWT_SECRET_KEY string `mapstructure:"JWT_SECRET_KEY"`
+	MODE           string `mapstructure:"MODE"`
+}
+
+func NewEnv() error {
+	path, _ := os.Getwd()
+	os.Chdir(path + "/../")
+	viper.SetConfigFile(".env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return fmt.Errorf("can't find the environment file : %v", err)
+	}
+
+	err = viper.Unmarshal(&Config)
+	if err != nil {
+		return fmt.Errorf("environment can't be loaded: %v", err)
+	}
+
+	envMap := map[string]string{
+		"PORT":           Config.PORT,
+		"DATABASE_URL":   Config.DATABASE_URL,
+		"JWT_SECRET_KEY": Config.JWT_SECRET_KEY,
+		"MODE":           Config.MODE,
+	}
+
+	for key, value := range envMap {
+		if value == "" {
+			return fmt.Errorf("environment variable %s is not set", key)
+		}
+	}
+
+	if Config.MODE == "development" {
+		log.Println("Server started in development mode")
+	}
+
+	return nil
+}
--- internal/database/db.go
+++ internal/database/db.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Shashank-Vishwakarma/code-pulse-backend/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DBClient *mongo.Client
+
+func Connect() error {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(config.Config.DATABASE_URL))
+	if err != nil {
+		return fmt.Errorf("failed to connect to database: %v", err)
+	}
+	DBClient = client
+	return nil
+}
