690ed69e0cdc9dfea5a393933e52bad5b4f8cc8b
Shashank <shashank78680@gmail.com>
2024-12-26 22:48:11 +0530

    camel case change in register request body

--- pkg/request/auth/request.go
+++ pkg/request/auth/request.go
@@ -5,7 +5,7 @@ type RegisterRequest struct {
 	Username        string `json:"username" validate:"required,min=6,max=20"`
 	Email           string `json:"email" validate:"required,email"`
 	Password        string `json:"password" validate:"required,min=8,max=20"`
-	ConfirmPassword string `json:"confirm_password" validate:"required,min=8,max=20"`
+	ConfirmPassword string `json:"confirmPassword" validate:"required,min=8,max=20"`
 }
 
 type LoginRequest struct {
