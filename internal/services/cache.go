1b7db3e40d3f02b0cb87b4ad110374934f31666c
Shashank <shashank78680@gmail.com>
2025-01-01 19:58:53 +0530

    set, get, invalidate cache functions added

--- internal/services/cache.go
+++ internal/services/cache.go
@@ -1,7 +1,9 @@
 package services
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/Shashank-Vishwakarma/code-pulse-backend/pkg/config"
 	"github.com/redis/go-redis/v9"
@@ -15,4 +17,37 @@ func InitializeRedis() {
 		Password: config.Config.REDIS_PASSWORD,
 		DB:       0, // use default DB
 	})
+}
+
+func SetCache(key string, value string) error {
+	ctx := context.Background()
+
+	result := RedisClient.Set(ctx, key, value, time.Hour*24)
+	if result.Err() != nil {
+		return result.Err()
+	}
+
+	return nil
+}
+
+func GetCache(key string) (string, error) {
+	ctx := context.Background()
+
+	value, err := RedisClient.Get(ctx, key).Result()
+	if err != nil {
+		return "", err
+	}
+
+	return value, nil
+}
+
+func InvalidateCache(key string) error {
+	ctx := context.Background()
+
+	result := RedisClient.Del(ctx, key)
+	if result.Err() != nil {
+		return result.Err()
+	}
+
+	return nil
 }
