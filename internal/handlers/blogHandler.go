33af4c651d64196aa936cdb5b61498aa4e358117
Shashank <shashank78680@gmail.com>
2025-01-02 23:06:42 +0530

    get all blogs api

--- internal/handlers/blogHandler.go
+++ internal/handlers/blogHandler.go
@@ -1,14 +1,20 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/Shashank-Vishwakarma/code-pulse-backend/internal/database"
 	"github.com/Shashank-Vishwakarma/code-pulse-backend/internal/models"
+	"github.com/Shashank-Vishwakarma/code-pulse-backend/pkg/config"
+	"github.com/Shashank-Vishwakarma/code-pulse-backend/pkg/constants"
 	request "github.com/Shashank-Vishwakarma/code-pulse-backend/pkg/request/auth"
 	"github.com/Shashank-Vishwakarma/code-pulse-backend/pkg/response"
 	"github.com/Shashank-Vishwakarma/code-pulse-backend/pkg/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func CreateBlog(c *gin.Context) {
@@ -49,7 +55,24 @@ func CreateBlog(c *gin.Context) {
 	response.HandleResponse(c, http.StatusCreated, "Blog created successfully", result)
 }
 
-func GetAllBlogs(c *gin.Context) {}
+func GetAllBlogs(c *gin.Context) {
+	options := options.Find().SetSort(bson.D{{"createdAt", -1}})
+	cursor, err := database.DBClient.Database(config.Config.DATABASE_NAME).Collection(constants.BLOG_COLLECTION).Find(context.TODO(), bson.M{}, options)
+	if err != nil {
+		logrus.Errorf("Error getting all blogs: GetAllBlogs API: %v", err)
+		response.HandleResponse(c, http.StatusInternalServerError, "Something went wrong", nil)
+		return
+	}
+
+	var blogs []models.Blog
+	if err := cursor.All(context.TODO(), &blogs); err != nil {
+		logrus.Errorf("Error decoding the blogs: GetAllBlogs API: %v", err)
+		response.HandleResponse(c, http.StatusInternalServerError, "Something went wrong", nil)
+		return
+	}
+
+	response.HandleResponse(c, http.StatusOK, "Blogs fetched successfully", blogs)
+}
 
 func GetBlogById(c *gin.Context) {}
 
