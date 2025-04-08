b2734bb2a13b77e0c7a7222ea010fb004d9003e0
Shashank <shashank78680@gmail.com>
2025-04-09 01:43:20 +0530

    password and confirm password min tag changed

--- pkg/request/auth/request.go
+++ pkg/request/auth/request.go
@@ -7,13 +7,13 @@ type RegisterRequest struct {
 	Name            string `json:"name" validate:"required,min=5,max=50"`
 	Username        string `json:"username" validate:"required,min=6,max=20"`
 	Email           string `json:"email" validate:"required,email"`
-	Password        string `json:"password" validate:"required,min=8,max=20"`
-	ConfirmPassword string `json:"confirmPassword" validate:"required,min=8,max=20"`
+	Password        string `json:"password" validate:"required,min=6,max=20"`
+	ConfirmPassword string `json:"confirmPassword" validate:"required,min=6,max=20"`
 }
 
 type LoginRequest struct {
 	Identifier string `json:"identifier" validate:"required"`
-	Password   string `json:"password" validate:"required,min=8,max=20"`
+	Password   string `json:"password" validate:"required,min=6,max=20"`
 }
 
 type VerifyEmailRequest struct {
